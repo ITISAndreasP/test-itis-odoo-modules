e68242431f892ea82734eda0f1443da5575fe667
Andreas Pohle <andreasp@itis,de>
2015-05-12 10:03:12 +0200

    copied the module to chec how the app store reacts

---
+++ itis-test-module-002/__openerp__.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "itis-test-module-002",
+
+    'summary': """
+        This is a short test for apps.odoo.com
+        """,
+
+    'description': """
+        This is a short test for apps.odoo.com
+    """,
+
+    'author': "IT IS AG",
+    'website': "http://www.itis-odoo.de",
+
+    # Categories can be used to filter modules in modules listing
+    # Check https://github.com/odoo/odoo/blob/master/openerp/addons/base/module/module_data.xml
+    # for the full list
+    'category': 'Uncategorized',
+    'version': '0.1',
+    'price': 99999.00,
+    'currency': 'EUR',
+
+    # any module necessary for this one to work correctly
+    'depends': ['base'],
+
+    # always loaded
+    'data': [
+        # 'security/ir.model.access.csv',
+        'templates.xml',
+    ],
+    # only loaded in demonstration mode
+    'demo': [
+        'demo.xml',
+    ],
+
+    'images': ['static/description/main_screenshot.png'],
+}
