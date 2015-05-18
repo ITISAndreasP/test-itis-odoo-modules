b9550514042b6138af65805eef14fd17101f7cd9
Andreas Pohle <andreasp@itis,de>
2015-05-18 11:27:29 +0200

    added a little bit more text

--- itis-test-module-001/__openerp__.py
+++ itis-test-module-001/__openerp__.py
@@ -18,8 +18,8 @@
     # for the full list
     'category': 'Uncategorized',
     'version': '0.1',
-    'price': 99999.00,
-    'currency': 'EUR',
+    #'price': 99999.00,
+    #'currency': 'EUR',
 
     # any module necessary for this one to work correctly
     'depends': ['base'],
